7c6b367bc7cff7576250e8232c7cc039f2573f46
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2022-11-13 00:34:00 +0530

    End of Section-4

--- StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -24,5 +24,20 @@ namespace StudentAdminPortal.API.Controllers
             return Ok(mapper.Map< List<Student>>(students));
 
         }
+
+        [HttpGet]
+        [Route("[controller]/{studentId:guid}")]
+        public async Task<IActionResult> GetStudentAsync([FromRoute] Guid studentId)
+        {
+            //Fetch Student Details
+            var student =await studentRepository.GetStudentAsync(studentId);
+
+            //Return Student
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(mapper.Map<Student>(student));
+        }
     }
 }
--- StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Program.cs
@@ -5,7 +5,12 @@ using StudentAdminPortal.API.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(
+    (options)=>
+    {
+        options.AddPolicy("angularApplication", (builder) => { builder.WithOrigins("http://localhost:4200").AllowAnyHeader().WithMethods("GET","POST","PUT","DELETE").WithExposedHeaders(); });
+    }
+    );
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,7 +31,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("angularApplication");
 app.UseAuthorization();
 
 app.MapControllers();
