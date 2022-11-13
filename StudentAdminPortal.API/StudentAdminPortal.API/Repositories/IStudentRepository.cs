e72ace9c0786326b67c12b1f565f23e82fde6957
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2022-11-13 13:05:47 +0530

    End of Section 6

--- StudentAdminPortal.API/StudentAdminPortal.API/Repositories/IStudentRepository.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/IStudentRepository.cs
@@ -9,5 +9,6 @@ namespace StudentAdminPortal.API.Repositories
         Task<List<Gender>> GetGendersAsync();
         Task<bool> Exists(Guid studentId);
         Task<Student> UpdateStudent(Guid studentId,Student request);
+        Task<Student> DeleteStudent(Guid studentId);
     }
 }
--- StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/SqlStudentRepository.cs
@@ -53,5 +53,17 @@ namespace StudentAdminPortal.API.Repositories
             }
             return null;
         }
+
+        public async Task<Student> DeleteStudent(Guid studentId)
+        {
+            var student=await GetStudentAsync(studentId);
+            if(student!=null)
+            {
+                context.Student.Remove(student);
+                await context.SaveChangesAsync();
+                return student;
+            }
+            return null;
+        }
     }
 }
